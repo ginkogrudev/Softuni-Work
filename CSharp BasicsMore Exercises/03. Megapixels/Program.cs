7b9a344e0bcdba0726985c4f10c4f049d1a69488
ginkogrudev <ginko404@gmail.com>
2017-06-03 19:11:34 +0300

    Added 3th and 4th task

---
+++ CSharp BasicsMore Exercises/03. Megapixels/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Megapixels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resolutionWidth = int.Parse(Console.ReadLine());
+            int resolutionHeight = int.Parse(Console.ReadLine());
+
+            double megapixels = Math.Round(resolutionWidth * resolutionHeight / 1000000.0, 1);
+            Console.WriteLine($"{resolutionWidth}x{resolutionHeight} => {megapixels}MP");
+        }
+    }
+}
